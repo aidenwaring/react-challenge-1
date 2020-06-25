43ed742132cd31df0d28c63085586bd6ac458314
aidenwaring <aiden.waring@icloud.com>
2020-06-25 13:47:01 +1000

    created HappyMessage function, seperated into component in tsx file.

---
+++ src/HappyMessage.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default (): JSX.Element => {
+  return (
+      <div>
+        <h1>Inspiration:</h1>
+        <h2>You can do it!</h2>
+        <h2>You're the best!</h2>
+        <h2>Good on you!</h2>
+        <h2>Nailed it!</h2>
+        <h2>You are awesome!</h2>
+      </div>
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HappyMessage from "./HappyMessage"
+
+ReactDOM.render(
+    <HappyMessage />,
+    document.getElementById("root")
+)
